d33af86909ceb5fd5e082a8f2ec734a3be766223
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2026-01-07 19:55:33 -0300

    feat: cria entidade de domínio Cliente

---
+++ src/main/java/com/company/vehicle_management/domain/entity/ClientEntity.java
@@ -0,0 +1,27 @@
+package com.company.vehicle_management.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String document;
+}
